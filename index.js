c23f029943a5633b2a8fde845ae02e29f3c6f8e8
CesarAugustoMor <cesaramfurst@gmail.com>
2020-01-28 14:51:02 -0200

    Adicionado express

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require( 'express' );
+const server = express();
+
+const users = [ 'Diego', 'Cláudio', 'Victor', 'César' ];
+
+server.get( '/users/:index', ( req, res ) =>
+{
+	const { index } = req.params;
+
+	return res.json( users[ index ] );
+} );
+
+server.listen( 3000 );
