3480864d5b259a0af6dd3b952d596e60bcd512a7
CesarAugustoMor <cesaramfurst@gmail.com>
2020-02-08 15:01:38 -0200

    adicionado middlewares

--- index.js
+++ index.js
@@ -1,14 +1,72 @@
-const express = require( 'express' );
+const express = require("express");
 
 const server = express();
+server.use(express.json());
 
-const users = [ 'Diego', 'Cláudio', 'Victor', 'César' ]; //vetor de usuarios cadastraos
+const users = ["Diego", "Cláudio", "Victor", "César"]; //vetor de usuarios cadastraos
 
-server.get( '/users/:index', ( req, res ) =>
-{
-	const { index } = req.params;
+server.use((req, res, next) => {
+  console.time("Tempo da Requisição");
 
-	return res.json( users[ index ] );
-} );
+  console.log("====================================");
+  console.log(
+    "Foi realizada uma requisição no método:",
+    req.method,
+    "Na rota/URL:",
+    req.url
+  );
 
-server.listen( 3000 );
+  next();
+
+  console.timeEnd("Tempo da Requisição");
+  console.log("====================================");
+});
+
+function checkUserExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: "Nome de Usuario requirido" });
+  }
+
+  return next();
+}
+
+function checkUserInArray(req, res, next) {
+  if (!users[req.params.index]) {
+    return res.status(400).json({ error: "Usuario não existente" });
+  }
+
+  return next();
+}
+
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+server.get("/users/:index", checkUserInArray, (req, res) => {
+  const { index } = req.params;
+
+  return res.json(users[index]);
+});
+
+server.post("/users", checkUserExists, (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+  return res.json(users);
+});
+
+server.put("/users/:idex", checkUserInArray, checkUserExists, (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete("/users/:index", checkUserInArray, (req, res) => {
+  const { index } = req.params;
+  // @ts-ignore
+  users.splice(index, 1);
+
+  return res.send();
+});
+server.listen(3000); //porta na qual o servidor está escutando
